f04bec4a1a92cd83004d615f1401694cd70d1ffa
chatton <github.qpeyb@simplelogin.fr>
2025-04-18 08:40:55 +0100

    fix: specify the grpc_laddr field in the embedded binary start args

--- cmd/celestia-appd/cmd/modify_root_command_multiplexer.go
+++ cmd/celestia-appd/cmd/modify_root_command_multiplexer.go
@@ -31,11 +31,23 @@ func modifyRootCommand(rootCommand *cobra.Command) {
 		ABCIVersion: abci.ABCIClientVersion1,
 		AppVersion:  3,
 		StartArgs: []string{
-			"--grpc.enable=true",
-			"--api.enable=true",
+			"--grpc.enable",
+			// ensure the grpc address is accessible from other hosts, not just locally.
+			"--grpc.address=0.0.0.0:9090",
+			"--api.enable",
 			"--api.swagger=false",
+			// we want to run the in standalone mode, as the comet node will be running natively in the multiplexer.
 			"--with-tendermint=false",
 			"--transport=grpc",
+			"--address=0.0.0.0:26658",
+			// the grpc_laddr field is required when starting the grpc server when running the embedded binary in standalone mode
+			// like this https://github.com/celestiaorg/celestia-app/blob/13020ab1d5861ce15f4caef5d06673a3dec8c78d/cmd/celestia-appd/cmd/start.go#L509
+			//
+			// this will then wire up the proxy server so requests to the BlockApi on 9090 are proxied to 9099 where there is a gRPC server running natively
+			// serving the BlockApi.
+
+			// The this happens here https://github.com/celestiaorg/cosmos-sdk/blob/49b16e81263be6ca1c493f79eb13832e246d3f2f/server/grpc/server.go#L41
+			"--rpc.grpc_laddr=tcp://0.0.0.0:9099",
 			// "--v2-upgrade-height=0",
 		},
 	})
